529d9a774f04a65816eddc3fb1d5141d9d94ffab
sigsant <sigfridsantamera@hotmail.com>
2018-07-01 19:12:15 +0200

    Actualizar logica del juego

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 var cartas = document.getElementsByTagName("img");
-var pareja = false;
 var gameOver = false;
 
 document.body.onclick = funcionCarta;
 
 function funcionCarta(){
     for(var i = 0; i < cartas.length; i++){
-        cartas[i].addEventListener("click", voltearCartas);
+        cartas[i].addEventListener("click", voltear);
     }
 }
 
-function voltearCartas(){
+function voltear(){
 
 var heart = document.getElementsByClassName("heart");
 var diamond = document.getElementsByClassName("diamond");
@@ -19,20 +18,28 @@ var clubs = document.getElementsByClassName("clubs");
      //Si las clases coinciden, voltea cada carta
     for(var x = 0; x < heart.length; x++){
         if(this == heart[x]){
-            console.log("esto es un corazon");
             this.src="../img/heart.png";
+            validarParejas(this, heart);
         }
         if(this == diamond[x]){
-            console.log("esto es un diamante");
             this.src="../img/diamond.png";
+            validarParejas(this, diamond);
         }
         if(this == spades[x]){
-            console.log("esto es una pica");
             this.src="../img/spades.png";
+            validarParejas(this, spades);
         }
         if(this == clubs[x]){
-            console.log("esto es un trébol");
-            this.src="../img/clubs.png";   
+            this.src="../img/clubs.png";  
+            validarParejas(this, clubs); 
         }
     }
-}   
+} 
+
+function validarParejas(esto, palo){
+    if(esto == palo[0]){
+        console.log("Primera pareja");
+    } else if(esto == palo[1]){
+       console.log("Segunda pareja");
+    }
+}
